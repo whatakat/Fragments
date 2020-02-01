76f8c04f4aa96011d862bf1c54ed78cb9debd597
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-01 21:54:12 +0300

    add CitiesFragment

---
+++ app/src/main/java/com/bankmtk/fragments/CitiesFragment.java
@@ -0,0 +1,21 @@
+package com.bankmtk.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.ListFragment;
+
+public class CitiesFragment extends ListFragment {
+    boolean isExistCoatofarms;
+    int currentPosition = 0;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container,false);
+    }
+}

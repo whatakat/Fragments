00c3e867e07a99c494efd1e87f42f9bd9a9cd5ad
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-02 16:50:09 +0300

    add CoatOfArmsActivity

---
+++ app/src/main/java/com/bankmtk/fragments/CoatOfArmsActivity.java
@@ -0,0 +1,25 @@
+package com.bankmtk.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
+
+public class CoatOfArmsActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getResources().getConfiguration().orientation ==
+        ORIENTATION_LANDSCAPE){
+            finish();
+            return;
+        }
+        if (savedInstanceState == null){
+            CoatOfArmsFragment details = new CoatOfArmsFragment();
+            details.setArguments(getIntent().getExtras());
+            //getFragmentManager().beginTransaction().add(android.R.content, details).commit();
+        }
+    }
+}

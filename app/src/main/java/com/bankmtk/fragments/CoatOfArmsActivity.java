39ef10d1c96a0187bdf6571b59430a8b42a82cd8
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-03 00:23:43 +0300

    change getFragmentManager to getSupportFragmentManager()

--- app/src/main/java/com/bankmtk/fragments/CoatOfArmsActivity.java
+++ app/src/main/java/com/bankmtk/fragments/CoatOfArmsActivity.java
@@ -5,10 +5,11 @@ import android.app.Fragment;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
 
-public class CoatOfArmsActivity extends Activity {
+public class CoatOfArmsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +21,7 @@ public class CoatOfArmsActivity extends Activity {
         if (savedInstanceState == null){
             CoatOfArmsFragment details = new CoatOfArmsFragment();
              details.setArguments(getIntent().getExtras());
-             //getFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
+             getSupportFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
 //question  api, build for 29
 
         }
